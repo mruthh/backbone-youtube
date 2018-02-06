495c3b6d46a398c21b747564d9227a87e03bc59f
Megan Ruth Honig <meganhonig@gmail.com>
2018-02-05 19:13:06 -0500

    add default search, clear previous searches when new term entered

--- views/AppView.js
+++ views/AppView.js
@@ -8,53 +8,60 @@ var AppView = Backbone.View.extend({
   initialize: function(){
     //listen for change in model's VideosCollection and create new views for each videoModel
     this.$videos = $('#videos-container');
-    // this.$thumbnails = $('#thumbnails-container');
+    this.$thumbnails = $('#thumbnails-container');
     this.listenTo(this.model.get('videos'), 'add', this.renderVideos);
-    this.listenTo(this.model.get('videos'), 'change:mainVideo', this.render)
+    this.listenTo(this.model.get('videos'), 'change:mainVideo', this.render);
+    //run search function with default terms on load
+    this.search('junior%20bulldog')
   },
 
   template: Handlebars.compile($('#main-video-template').html()),
 
   renderVideos: function(video){
+    //set first result's mainVideo attribute to true
+    appModel.get('videos').at(0).set('mainVideo', true)
     //when new video added, create new view for that video, then append to videos list
     var videoView = new VideoView({ model: video});
-    this.$thumbnails = this.$el.find('#thumbnails-container');
-    console.log(this.$thumbnails)
     this.$thumbnails.append(videoView.render().el);
   },
 
   render: function(){
-    //set mainVideo, append main video to videos container, then append thumbnails
-    this.$videos.empty();
     this.mainVideo = appModel.get('videos').findWhere({mainVideo: true});
-    //put first element in container into main video template
-    this.$videos.prepend(this.template(this.mainVideo.attributes))
-    //remove current main video from sidebar OR get all videos and add to sidebar
-    this.sidebarVideos = appModel.get('videos').where({mainVideo: false});
-
+    //only change view when a new mainVideo has been added
+    if(this.mainVideo){
+      //set mainVideo, append main video to videos container, then append thumbnails
+      this.$videos.empty();
+      //put first element in container into main video template
+      this.$videos.append(this.template(this.mainVideo.attributes))
+      //remove current main video from sidebar OR get all videos and add to sidebar
+      appModel.get('videos')
+    }
     return this;
   },
+  search: function(input){
+    // update collection URL and fetch videos
+    this.model.searchTerm = input;
+    this.searchUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC8fgos94ydLf_pDTtLOIIP8HcEhdrSxtM&part=snippet&type=video&q=${input}`;
+    this.videos = appModel.get('videos')
+    //first, remove all videos previously in collection
+    this.videos.remove(this.videos.toArray())
+    this.videos.url = this.searchUrl;
+    this.videos.fetch({
+      success: function(){
+      },
+      error: function(){
+        alert('No videos found. Please check your search terms and try again.')
+      }
+    });
 
+  },
   searchOnEnter: function(e){
-    //when user enters search term, update collection URL and fetch videos
-    //does collection need to be emptied first??
+    //when user enters search term, validate their input
+    //if input is valid, search using input
     this.$input = $('#search').val();
-    if (this.$input && e.keyCode === 13){ //anything else we need to check for to validate?
-      this.model.searchTerm = this.$input;
-      this.searchUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyC8fgos94ydLf_pDTtLOIIP8HcEhdrSxtM&part=snippet&q=${this.$input}`;
-      var videos = appModel.get('videos')
-      videos.url = this.searchUrl;
-      videos.fetch({
-        success: function(){
-      //set first result's mainVideo attribute to true
-          videos.at(0).set('mainVideo', true)
-        },
-        error: function(){
-          alert('No videos found. Please try again.')
-        }
-      });
-
-
+    if (this.$input && e.keyCode === 13){
+      //can we validate that user input is url-safe?
+      this.search(this.$input);
     }
   }
 
--- views/VideoView.js
+++ views/VideoView.js
@@ -1,15 +1,42 @@
 var VideoView = Backbone.View.extend({
+  className: 'align-middle',
   events:{
     //listen for clicks on thumbnail. when clicked, set mainVideo to true. find previous mainVideo and set to false.
+    'click': 'changeMainVideo'
   },
   initialize: function(){
-
+    //if mainVideo attribute changes, show or hide thumbnail
+    this.listenTo(this.model, 'change:mainVideo', this.toggleHide)
+    //if model is removed, remove view
+    this.listenTo(this.model, 'remove', this.removeVideoView)
   },
   template: Handlebars.compile($('#thumbnail-template').html()),
 
+  removeVideoView: function(){
+    console.log('removed')
+    this.remove();
+  },
   render: function(){
     this.$el.html(this.template(this.model.attributes))
+    //if this video is set as main, hide its thumbnail
+    if(this.model.get('mainVideo')){
+      this.$el.addClass('d-none')
+    }
     return this;
+  },
+
+  toggleHide: function(){
+    this.$el.toggleClass('d-none');
+  },
+
+  changeMainVideo: function(){
+    //first, change existing mainVideo to false
+    var currentMainVideo = appModel.get('videos').findWhere({mainVideo:true})
+      if (currentMainVideo){
+      currentMainVideo.set('mainVideo', false);
+    }
+    //then change clicked video's mainVideo attribute to true
+    this.model.set('mainVideo', true);
   }
 
 });
